1db9f9d69945bf0e7a94929ba3c76774ea9c2af3
Carlos Filipe da Silva Oliveira <dasilva.carlos@outlook.com>
2017-01-22 15:12:40 +0100

    Updated examples AssemblyInfo.cs

--- CarlosInIt.EntityFramework.Mocks.Examples/Properties/AssemblyInfo.cs
+++ CarlosInIt.EntityFramework.Mocks.Examples/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following set of attributes.
 // Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("CarlosInIt.EntityFramework.Mocks.Examples")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Contains examples on how to use the mocks")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("CarlosInIt")]
 [assembly: AssemblyProduct("CarlosInIt.EntityFramework.Mocks.Examples")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © CarlosInIt 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
